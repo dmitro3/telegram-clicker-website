bde52d82c5b4cd9b079d6e2239d68895caabe9e9
Vadym Pershoguba <tanakenrip@gmail.com>
2024-06-03 16:37:41 +0200

    g

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use('/getGameData', getGameData);
 
 //db.run("CREATE TABLE IF NOT EXISTS users_data (telegramId INTEGER PRIMARY KEY, energy INTEGER, coins INTEGER, time TEXT)");
 
-db.run("DROP TABLE users_data");
+//db.run("DROP TABLE users_data");
 
 db.run("CREATE TABLE IF NOT EXISTS users_data (ID INTEGER PRIMARY KEY, telegramId INTEGER, energy INTEGER, coins INTEGER, time TEXT)");
