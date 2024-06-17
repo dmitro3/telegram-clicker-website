399e5e260a4daf558f01c455c704a5853a903c31
Vadym Pershoguba <tanakenrip@gmail.com>
2024-06-17 16:55:38 +0200

    change

--- website/script.js
+++ website/script.js
@@ -979,6 +979,7 @@ function adjustMineCards() {
   .then(data => {
     if (data.data != 'User added') {
       const information = Array.from(data.data)[0]
+      console.log(information)
       showCurrentMineCards(information);
     }
   });
