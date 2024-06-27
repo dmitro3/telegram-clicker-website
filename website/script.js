287f19b115d84ee4aa38bf876a5c998a23e21aa8
Vadym Pershoguba <tanakenrip@gmail.com>
2024-06-27 13:17:51 +0200

    bbbbbb

--- website/script.js
+++ website/script.js
@@ -361,9 +361,7 @@ function showClick(event) {
     plusOne.style.position = "absolute";
     plusOne.style.left = (x-20) + "px";
     plusOne.style.top = (y-20) + "px";
-    plusOne.style.color = 'green'
     plusOne.style.zIndex = '5';
-    plusOne.style.fontSize = '36px'
     plusOne.style.animation = "upAndFadeOut 1s forwards"; 
     gameField.style.animation = 'none'
     gameField.offsetHeight;
