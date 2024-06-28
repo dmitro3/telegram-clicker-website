08d13bc54fd852c80a04a1ac76363fd5b5e24724
Vadym Pershoguba <tanakenrip@gmail.com>
2024-06-28 11:55:24 +0200

    rrr

--- routes/updateGameData.js
+++ routes/updateGameData.js
@@ -4,21 +4,41 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('mydatabase.db');
 
 router.post('/', async (req, res, next) => {
-    data = req.body;
-    await updateGameData(data)
-    res.json({})
+    const data = req.body;
+    try {
+        await updateGameData(data);
+        res.json({ success: true });
+    } catch (err) {
+        console.error("Error inserting data:", err.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-async function updateGameData(data) {
+async function updateGameData(data, retriesLeft = 3) {
+    const query = "INSERT INTO users_data (telegramId, energy, coins, time) VALUES (?, ?, ?, ?)";
+    const params = [+data.telegramId, data.energy, data.coins, data.time];
+
     return new Promise((resolve, reject) => {
-        db.run("INSERT INTO users_data (telegramId, energy, coins, time) VALUES (?, ?, ?, ?)", [+data.telegramId, data.energy, data.coins, data.time], function (err) {
-            if (err) {
-                console.error("Error inserting data:", err.message);
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    } 
-    )} 
+        const attemptInsert = () => {
+            db.run(query, params, function (err) {
+                if (err) {
+                    if (err.code === 'SQLITE_BUSY' && retriesLeft > 0) {
+                        console.log('Database is busy, retrying...');
+                        setTimeout(() => {
+                            attemptInsert();
+                        }, 100); // Retry after 100ms
+                    } else {
+                        console.error("Error inserting data:", err.message);
+                        reject(err);
+                    }
+                } else {
+                    resolve();
+                }
+            });
+        };
+
+        attemptInsert();
+    });
+}
+
 module.exports = router;
