89decd921270e69f6bcd47070ee99ca31a3c071a
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-01 13:44:22 +0200

    yyyyyy

--- routes/getReferrals.js
+++ routes/getReferrals.js
@@ -5,13 +5,13 @@ const db = new sqlite3.Database('mydatabase.db');
 
 router.post('/', async (req, res, next) => {
     const data = req.body;
-    const rows = await getGameData(data.telegramSourceId)
-    console.log(rows)
+    const rows = await getGameData(+data.telegramSourceId)
+    console.log('ROWS '+ rows)
     res.json({'data': rows})
 });
 
 async function getGameData(telegramId) {
-    console.log(telegramId)
+    console.log('ID ' + telegramId)
     return new Promise((resolve, reject) => {
         db.all(`SELECT * FROM referrals_data WHERE telegramSourceId = ?`,[telegramId] , function (err, rows) {
             if (err) {
