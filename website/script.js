07de475238d2c920d4a658f3502c867ce0f02a5b
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-01 13:15:56 +0200

    rrrrr

--- website/script.js
+++ website/script.js
@@ -66,6 +66,7 @@ window.onload = ()=> {
 
 function adjustReferrals () {
   const currentUrl = `${window.location.href}`;
+  console.log('url - ' + currentUrl)
   const code = currentUrl.match(/start=([^#]*)#/)[1];
   console.log('code - ' + code)
   if (code) {
