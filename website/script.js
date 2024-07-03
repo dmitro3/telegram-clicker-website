53df0dbca5152588c75defce017f4f5abe0cb06b
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-03 12:48:53 +0200

    xcxcxcxc

--- website/script.js
+++ website/script.js
@@ -1041,7 +1041,7 @@ function showMineField() {
   earnPerClickLabel.style.marginTop = '67px';
   passiveClicksCoin.style.marginTop = '67px';
   passiveClicksLabel.style.marginTop = '67px';
-  gameField.style.height = '1100px';
+  gameField.style.height = '1200px';
   gameField.style.marginTop = '16px';
   earnPerClickBox.style.marginTop = '36px'
   clicksTillLevelUp.style.marginTop = '36px'
