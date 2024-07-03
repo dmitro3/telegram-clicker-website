98ba4ee916f17244d24c3ff8cab41e6b45ff9619
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-03 11:47:35 +0200

    adfvvvvvv

--- website/script.js
+++ website/script.js
@@ -19,6 +19,14 @@ function handleOrientationChange() {
   }
 }
 
+window.addEventListener('orientationchange', function ()
+{
+    if (window.innerHeight > window.innerWidth)
+    {
+        document.getElementsByTagName('body')[0].style.transform = "rotate(90deg)";
+    }
+});
+
 // Initial call to handle orientation based on current state
 handleOrientationChange();
 
