11a7d5675a30948841442d322e194b41a0f8ccd1
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-08 15:33:00 +0200

    rrrrrr

--- routes/updateGameData.js
+++ routes/updateGameData.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const GameData = require('../models/GameData');
 
+// POST route to insert or update game data
 router.post('/', async (req, res) => {
+    const { telegramId } = req.body;
+
     try {
-        const gameData = new GameData(req.body);
+        let gameData = await GameData.findOne({ telegramId });
+
+        if (gameData) {
+            // Update existing record
+            gameData.coins = req.body.coins;
+            gameData.energy = req.body.energy;
+            gameData.time = req.body.time;
+        } else {
+            // Insert new record
+            gameData = new GameData({
+                telegramId: req.body.telegramId,
+                coins: req.body.coins,
+                energy: req.body.energy,
+                time: req.body.time
+            });
+        }
+
         const savedGameData = await gameData.save();
-        res.status(201).json(savedGameData);
+        res.status(200).json(savedGameData);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
