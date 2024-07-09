0e7fb9434899e9f64d4eea3a7ffbe7edde4829fe
Vadym Pershoguba <tanakenrip@gmail.com>
2024-07-09 15:23:24 +0200

    ouuuuiiii

--- models/MineCards.js
+++ models/MineCards.js
@@ -5,7 +5,7 @@ const mineCardsShema = new mongoose.Schema({
     funTokensBox: { type: Number, required: true },
     stakingBox: { type: Number, required: true },
     btcPairsBox: { type: Number, required: true },
-    ethPartsBox: { type: Number, required: true },
+    ethPairsBox: { type: Number, required: true },
     top10CMCBox: { type: Number, required: true },
     gameFiBox: { type: Number, required: true },
     defiBox: { type: Number, required: true },
